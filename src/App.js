b0bbd21070515dc78765f54b7bf39f6e15305a06
Javier Bonaventura <bonaventura.javier@gmail.com>
2024-03-03 17:41:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import NavBar from './Components/NavBar';
 import Usuarios from './Components/Usuarios';
 import Productos from './Components/Productos';
-import Inicio from './Components/Inicio'
+import Inicio from './Components/Inicio';
 
 function App() {
+  const location = useLocation();
+  const rutaActiva = location.pathname
+  console.log(location.pathname)
+  
+
   return (
-    <Router>
+
       <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
         <NavBar />
         <div style={{ display: 'flex', flex: 1 }}>
-          <Sidebar />
+          <Sidebar rutaActiva={rutaActiva} />
           <div style={{ flex: 1, padding: '20px' }}>
             <Routes>
               <Route path="/" element={<Inicio />} />
@@ -22,17 +27,8 @@ function App() {
           </div>
         </div>
       </div>
-    </Router>
+
   );
 }
-
-// function Inicio() {
-//   return (
-//     <div>
-//       <h2>Inicio</h2>
-//       <p>Bienvenido a la página de inicio</p>
-//     </div>
-//   );
-// }
 
 export default App;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Sidebar() {
+function Sidebar({rutaActiva}) {
   return (
     <div style={{ width: '200px', background: '#f0a0f0' }}>
       <h3>Sidebar</h3>
       <ul>
         <li><Link to="/usuarios">Usuarios</Link></li>
         <li><Link to="/productos">Productos</Link></li>
+        <h3>{rutaActiva}</h3>
       </ul>
     </div>
   );
